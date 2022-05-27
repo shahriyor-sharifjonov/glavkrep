77721fefc765031daa37fbc2e6bfd8bc340c7933
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-05-27 17:17:06 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,8 @@ functions.isWebp();
 
 // const swiper = new Swiper();
 
+import $ from 'jquery'
+
 const headerButton = document.querySelector(".header__left");
 const headerTxt = document.querySelector('.header__left p');
 const headerMenu = document.querySelector(".header__list");
@@ -47,3 +49,21 @@ window.onclick = (e) => {
   )
     katalogToggle();
 };
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
